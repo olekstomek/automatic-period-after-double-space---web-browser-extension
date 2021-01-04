0f65a38212a64a8883a6d380dedc40582b82a6c6
olekstomek <olekstomek@gmail.com>
2021-01-04 13:42:51 +0100

    fix cursor position when you use double space in the middle of the text (cursor was moved on the end of text); the script does not change the values in password fields

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,7 @@
-console.log('foreground works works first!');
-(function() {
-    console.log('foreground works!');
-    document.addEventListener('keyup', function(event) {
-        var element = event.target;
-        var activeInputFieldSelector = element.tagName + ":nth-child(" + indexOf(element) + ")";
+(function () {
+    document.addEventListener('keypress', function (event) {
+        element = event.target;
+        activeInputFieldSelector = element.tagName + ":nth-child(" + indexOf(element) + ")";
         while ((element = element.parentElement) != null) {
             if (element.tagName === "BODY") {
                 activeInputFieldSelector = "BODY > " + activeInputFieldSelector;
@@ -11,21 +9,14 @@ console.log('foreground works works first!');
             }
             activeInputFieldSelector = element.tagName + ":nth-child(" + indexOf(element) + ") > " + activeInputFieldSelector;
         }
-        console.log(activeInputFieldSelector);
-
-        if (event.keyCode === 32) {
-            var originalValue = document.querySelector(activeInputFieldSelector).value.trimStart();
-            console.log(originalValue);
-            var correctedValue = originalValue.replaceAll("  ", ". ");
-            console.log(correctedValue);
-            document.querySelector(activeInputFieldSelector).value = correctedValue;
-        } else {
-            return;
-        }
+        
+        setTimeout(function () {
+            replaceText(activeInputFieldSelector, "  ", ". ");
+        }, 0)
     });
 
     function indexOf(element) {
-        var parent = element.parentNode;
+        parent = element.parentNode;
         var child, index = 1;
         for (child = parent.firstElementChild; child; child = child.nextElementSibling) {
             if (child === element) {
@@ -35,4 +26,21 @@ console.log('foreground works works first!');
         }
         return -1;
     }
-})();
+
+    function getCount(str, search) {
+        return str.split(search).length - 1;
+    }
+
+    function replaceText(activeInputFieldSelector, search, replaceWith) {
+        activeInput = document.querySelector(activeInputFieldSelector);
+        if (activeInput.value.indexOf(search) >= 0 && activeInput.type !== 'password') {
+            start = activeInput.selectionStart;
+            end = activeInput.selectionEnd;
+            textBefore = activeInput.value.substr(0, end);
+            lengthDiff = (replaceWith.length - search.length) * getCount(textBefore, search);
+            activeInput.value = activeInput.value.replace(search, replaceWith);
+            activeInput.selectionStart = start + lengthDiff;
+            activeInput.selectionEnd = end + lengthDiff;
+        }
+    }
+}())
