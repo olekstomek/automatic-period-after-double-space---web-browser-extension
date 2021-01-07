20b47cf27ec03ea32bbabf68936b165a16056dc4
olekstomek <olekstomek@gmail.com>
2021-01-07 09:39:26 +0100

    small refactor

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 (function () {
     document.addEventListener('keypress', function (event) {
-        element = event.target;
-        activeInputFieldSelector = element.tagName + ":nth-child(" + indexOf(element) + ")";
+        let element = event.target;
+        let activeInputFieldSelector = element.tagName + ":nth-child(" + indexOf(element) + ")";
         while ((element = element.parentElement) != null) {
             if (element.tagName === "BODY") {
                 activeInputFieldSelector = "BODY > " + activeInputFieldSelector;
@@ -9,15 +9,15 @@
             }
             activeInputFieldSelector = element.tagName + ":nth-child(" + indexOf(element) + ") > " + activeInputFieldSelector;
         }
-        
+
         setTimeout(function () {
             replaceText(activeInputFieldSelector, "  ", ". ");
         }, 0)
-    });
+    })
 
     function indexOf(element) {
-        parent = element.parentNode;
-        var child, index = 1;
+        let parent = element.parentNode;
+        let child, index = 1;
         for (child = parent.firstElementChild; child; child = child.nextElementSibling) {
             if (child === element) {
                 return index;
@@ -32,12 +32,12 @@
     }
 
     function replaceText(activeInputFieldSelector, search, replaceWith) {
-        activeInput = document.querySelector(activeInputFieldSelector);
+        let activeInput = document.querySelector(activeInputFieldSelector);
         if (activeInput.value.indexOf(search) >= 0 && activeInput.type !== 'password') {
             start = activeInput.selectionStart;
-            end = activeInput.selectionEnd;
-            textBefore = activeInput.value.substr(0, end);
-            lengthDiff = (replaceWith.length - search.length) * getCount(textBefore, search);
+            let end = activeInput.selectionEnd;
+            let textBefore = activeInput.value.substr(0, end);
+            let lengthDiff = (replaceWith.length - search.length) * getCount(textBefore, search);
             activeInput.value = activeInput.value.replace(search, replaceWith);
             activeInput.selectionStart = start + lengthDiff;
             activeInput.selectionEnd = end + lengthDiff;
